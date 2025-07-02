ef429cb99c9032439fb478ea80cc5acba498b5c4
Alejandro Hervás <alesepto15@gmail.com>
2025-07-02 23:59:44 +0200

    feat: enhance PlayScene with player animations and input handling

--- src/game/PlayScene.ts
+++ src/game/PlayScene.ts
@@ -5,14 +5,55 @@ export default class PlayScene extends Phaser.Scene {
     super("PlayScene");
   }
 
+  private player!: Phaser.Physics.Arcade.Sprite;
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+
   create() {
     const { width, height } = this.scale;
 
-    this.add.text(20, 20, "Nivel 1 - Muestra de proyectos", {
-      fontSize: "20px",
-      color: "#ffffff",
+    this.player = this.physics.add.sprite(width / 2, height / 2, "dude");
+
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
     });
 
-    this.add.image(width / 2, height / 2, "logo").setScale(0.5);
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.cursors = this.input?.keyboard?.createCursorKeys();
+  }
+
+  update() {
+    if (!this.cursors || !this.player) return;
+
+    if (this.cursors.left?.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right?.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up?.isDown && this.player.body?.touching?.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
